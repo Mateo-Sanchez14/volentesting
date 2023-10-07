547a285a4cae65c7ff09a09d55e1519a48fcdd75
Mateo <mateo-sanchez@hotmail.com>
2023-10-07 09:51:07 -0300

    add first test

---
+++ test/lista.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2023, Laboratorio de Microprocesadores
+ * Facultad de Ciencias Exactas y Tecnología, Universidad Nacional de Tucumán
+ * https://www.microprocesadores.unt.edu.ar/
+ *
+ * Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+const { assert } = require("chai");
+
+const describe = require("mocha").describe;
+const it = require("mocha").it;
+
+describe("In the list of pairs key-value ...", () => {
+  describe("When the list is empty", () => {
+    it("There is cero elements", () => {
+      assert.fail("Not implemented yet");
+    });
+  });
+});
